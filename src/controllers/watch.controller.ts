10e9dd83c506387c11f470a0abb28e562d90b0ea
SaadkamalShaikhdev <saadkamaldev@gmail.com>
2026-06-21 23:56:01 +0500

    Make a Resuable prisma components

--- src/controllers/watch.controller.ts
+++ src/controllers/watch.controller.ts
@@ -1,12 +1,11 @@
 import { Router, Request, Response } from "express";
 import { PrismaClient } from "../../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { prisma } from "../lib/prisma.js";
 import dotenv from "dotenv";
 dotenv.config();
 const router = Router();
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
-});
+
 
 export const getWatches = async (req: Request, res: Response) => {
   try {
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import express from "express";
-import { PrismaClient } from "../generated/prisma/client.js";
-import { PrismaPg } from "@prisma/adapter-pg";
-import dotenv from "dotenv";
+
 import watchRoutes from "./routes/watch.route.js";
 
 const app = express();
 const PORT = 5000;
 
-dotenv.config();
-app.use(express.json());
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
-});
+
 app.get("/", (req, res) => {
   res.send("Watch Management API Running");
 });
